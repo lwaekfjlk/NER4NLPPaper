08de93d85447704ed2760f8a10594eb37a36cbc1
lwaekfjlk <1125027232@qq.com>
2022-11-04 17:55:35 -0400

    modify dataset

--- ner_baseline/dataset.py
+++ ner_baseline/dataset.py
@@ -13,10 +13,10 @@ class SciNERDataset(Dataset):
         self.sep = self.tokenizer.sep_token_id
         self.pad = self.tokenizer.pad_token_id
         file_dict = {'train': args.train_file, 'dev': args.dev_file, 'test': args.test_file}
-        self.data = self._read_conll_file(file_dict[split], sep)
+        self.data = self.collect_data(file_dict[split], sep)
 
 
-    def _read_conll_file(self, file, sep):
+    def collect_data(self, file, sep):
         data = []
         sent = []
         with open(file, 'r') as f:
@@ -24,27 +24,27 @@ class SciNERDataset(Dataset):
         for l in lines:
             l = l.strip()
             if len(l) == 0:
-                instance = self._create_example(sent, sep=sep)
+                instance = self.collect_instance(sent, sep=sep)
                 data.append(instance)
                 sent = []
             else:
                 sent.append(l)
         return data
 
-    def _create_example(self, conll_sentence, sep=' -X- _ '):
-        example = {'input_ids': [], 'token_starts': [], 'labels': []}
+    def collect_instance(self, conll_sentence, sep=' -X- _ '):
+        instance = {'input_ids': [], 'token_starts': [], 'labels': []}
         for line in conll_sentence:
             token, tag = line.split(sep)
             token_ids = self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize(token))
             label = self.entity2id[tag]
             labels = [self.entity2id[tag]]
             token_starts = [1] + [0 for _ in range(len(token_ids) - 1)]
-            example['input_ids'] += token_ids
-            example['token_starts'] += token_starts
-            example['labels'] += labels
-        example['input_ids'] = [self.cls] + example['input_ids'] + [self.sep]
-        example['token_starts'] = [0] + example['token_starts'] + [0]
-        return example
+            instance['input_ids'] += token_ids
+            instance['token_starts'] += token_starts
+            instance['labels'] += labels
+        instance['input_ids'] = [self.cls] + instance['input_ids'] + [self.sep]
+        instance['token_starts'] = [0] + instance['token_starts'] + [0]
+        return instance
 
     def __len__(self):
         return len(self.data)
